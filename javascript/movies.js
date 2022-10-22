89291963a5aa8fa452b214a18ddf3cd5659c8f10
tfotr241290 <franco_9024@hotmail.com>
2022-10-22 18:37:16 -0500

    play trailer

--- javascript/movies.js
+++ javascript/movies.js
@@ -2,6 +2,8 @@ const url = 'https://api.themoviedb.org/3/movie/436270?api_key=4125d5c1ddacae373
 
 const link = 'https://api.themoviedb.org/3/movie/436270/credits?api_key=4125d5c1ddacae373a5219254ef25e65'
 
+const videoLink = 'https://www.youtube.com/watch?v=a1mcS4tKGNg'
+
 const requestMOvie = async(url,link) =>{
 
     const response = await fetch(url);
@@ -15,6 +17,7 @@ const requestMOvie = async(url,link) =>{
         console.log(data);
         console.log(data2)
         displayData(data,data2);
+        document.querySelector('#trailer').addEventListener('click',playTrailer());
     }
     else if(response.status === 401){
         console.log('Ups, something went wrong...')
@@ -29,11 +32,14 @@ const displayData = (data,data2) =>{
     results2 = data2;
     const content = `<img class="movie-img"src="https://image.tmdb.org/t/p/w500/${results.poster_path}" alt="Image of ${results.title}">
     <h1>${results.title} (${results.release_date})</h1>
-    <p class="genres">Genres: ${results.genres[0].name}, ${results.genres[1].name}, ${results.genres[2].name}.</p>
+    <p class="genres">Genres: ${results.genres[0].name}, ${results.genres[1].name}, ${results.genres[2].name}. <button id="trailer">PLAY TRAILER</button></p>
     <section class="overview"> <h3>Overview</h3><p>${results.overview}</p></section> <section class="revenue"><h3>Revenue</h3><p>$ ${results.revenue}</p></section> <section class="pop"><h3>Popularity</h3><p>${results.popularity}</p></section> <section class="dur"><h3>Duration</h3><p>${results.runtime} min</p></section> <section class="cast"><p>Cast: ${results2.cast[0].name}, ${results2.cast[1].name}, ${results2.cast[2].name}, ${results2.cast[3].name}, ${results2.cast[4].name}.</p></section>`;
     document.querySelector('#container').innerHTML = content;
     
 }
 
+const playTrailer = ()=>{
+    window.open('https://www.youtube.com/watch?v=a1mcS4tKGNg','_blank');
+}
 
 requestMOvie(url,link);
